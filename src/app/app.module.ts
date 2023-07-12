fb7321fb90be94d3cc41eb4fa5a885853ba725fc
cinta <cintatafur@gmail.com>
2023-07-12 10:05:48 +0200

    ➕ Create(Router Module & pages)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { SharedModule } from './shared/shared.module';
-
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-roting.module';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, SharedModule],
+  imports: [AppRoutingModule, BrowserModule, SharedModule],
   providers: [],
   bootstrap: [AppComponent],
 })
