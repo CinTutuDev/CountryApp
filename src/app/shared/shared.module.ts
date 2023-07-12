20d1ad2aa8125840e155b38406062c3145b09ff1
cinta <cintatafur@gmail.com>
2023-07-12 10:48:35 +0200

    ✨ Router Module & RouterLinkOptions

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,10 +3,23 @@ import { CommonModule } from '@angular/common';
 
 import { AboutPageComponent } from './pages/about-page/about-page.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { RouterModule } from '@angular/router';
+import { ContactPageComponent } from './pages/contact-page/contact-page.component';
 
 @NgModule({
-  declarations: [AboutPageComponent, HomePageComponent],
-  imports: [CommonModule],
-  exports: [AboutPageComponent, HomePageComponent],
+  declarations: [
+    AboutPageComponent,
+    ContactPageComponent,
+    HomePageComponent,
+    SidebarComponent,
+  ],
+  imports: [CommonModule, RouterModule],
+  exports: [
+    AboutPageComponent,
+    ContactPageComponent,
+    HomePageComponent,
+    SidebarComponent,
+  ],
 })
 export class SharedModule {}
