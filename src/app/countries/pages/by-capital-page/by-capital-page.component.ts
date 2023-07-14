6c7bbb4c64f467568f577e833b72d27e99dfeaf4
cinta <cintatafur@gmail.com>
2023-07-14 08:52:25 +0200

    🧐 Muestro(capitales)

--- src/app/countries/pages/by-capital-page/by-capital-page.component.ts
+++ src/app/countries/pages/by-capital-page/by-capital-page.component.ts
@@ -1,16 +1,22 @@
 import { Component } from '@angular/core';
+import { ContriesService } from '../../services/countries.service';
+import { Country } from '../../interfaces/country';
 
 @Component({
   selector: 'app-by-capital-page',
   templateUrl: './by-capital-page.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class ByCapitalPageComponent {
+  public countries: Country[] = [];
 
-  searchByCapital(term: string): void{
+  constructor(private countriesS: ContriesService) {}
+
+  searchByCapital(term: string): void {
+    this.countriesS.searchCapital(term).subscribe((countries) => {
+      this.countries = countries;
+    });
     console.log('Desde ByCapitalPage');
-    console.log({term});
+    console.log({ term });
   }
-
 }
