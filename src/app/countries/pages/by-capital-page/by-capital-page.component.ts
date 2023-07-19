3c811da2ef8789aa1c31b75aa4a300137fb5f775
cinta <cintatafur@gmail.com>
2023-07-19 12:54:47 +0200

    🕶LoadingSpinner(muestro y oculto)

--- src/app/countries/pages/by-capital-page/by-capital-page.component.ts
+++ src/app/countries/pages/by-capital-page/by-capital-page.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 import { ContriesService } from '../../services/countries.service';
 import { Country } from '../../interfaces/country';
 
-
 @Component({
   selector: 'app-by-capital-page',
   templateUrl: './by-capital-page.component.html',
@@ -10,14 +9,15 @@ import { Country } from '../../interfaces/country';
 })
 export class ByCapitalPageComponent {
   public countries: Country[] = [];
+  public isLoading: boolean = false;
 
   constructor(private countriesS: ContriesService) {}
 
   searchByCapital(term: string): void {
+    this.isLoading = true;
     this.countriesS.searchCapital(term).subscribe((countries) => {
       this.countries = countries;
+      this.isLoading = false;
     });
-    /* console.log('Desde ByCapitalPage');
-    console.log({ term }); */
   }
 }
