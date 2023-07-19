9c3cbed3a9999ceabc1cebc7cb1c947581380670
cinta <cintatafur@gmail.com>
2023-07-19 15:34:45 +0200

    💡Buscar por region

--- src/app/countries/pages/by-region-page/by-region-page.component.ts
+++ src/app/countries/pages/by-region-page/by-region-page.component.ts
@@ -2,23 +2,23 @@ import { Component } from '@angular/core';
 import { ContriesService } from '../../services/countries.service';
 import { Country } from '../../interfaces/country';
 
+type Region = 'Africa' | 'Americas' | 'Asia' | 'Europe' | 'Oceania';
 @Component({
   selector: 'app-by-region-page',
   templateUrl: './by-region-page.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class ByRegionPageComponent {
+  public countries: Country[] = [];
+  public region: Region[] = ['Africa', 'Americas', 'Asia', 'Europe', 'Oceania'];
+  public selectedRegion?: Region;
 
-  public region: Country[] = [];
   constructor(private countriesS: ContriesService) {}
 
-
-  searchByRegion(term: string): void {
-    this.countriesS.searchRegion(term).subscribe((countries) => {
-      this.region = countries;
+  searchByRegion(region: Region): void {
+    this.selectedRegion = region;
+    this.countriesS.searchRegion(region).subscribe((countries) => {
+      this.countries = countries;
     });
-    /* console.log('Desde ByCapitalPage');
-    console.log({ term }); */
   }
 }
